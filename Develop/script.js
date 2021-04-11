22a672d49485e0a9fa7d20ff71968f6db0f6a715
ME <mebener44@gmail.com>
2021-04-11 10:43:54 -0700

    added variable for capital letters

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,7 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate"); 
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -19,6 +20,21 @@ generateBtn.addEventListener("click", writePassword);
 //Function to alert when button is clicked //
 
 
+var capitalLetters = function () {
+  var capital = "";
+  capital = true;
+window.confirm("Do you want capital letters?");
+return capital;
+
+}
+
+// var x = window.confirm("Do you want capital letters?")
+  // confirm capital true or false
+// var x = window.confirm("Do you want to use symbols?")
+  //confirm symbols true or false
+// var x = prompt("Choose a password length between 8 & 120")
+  //log the number of characters
+
 
 
 //alerts to reference including case, symbols & length
@@ -26,5 +42,3 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-
-var generateBtn = window.alert("You need to capitalize");
