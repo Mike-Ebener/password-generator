09ba25cd47f5a81b6b484f1e30dba5b5f0400d4e
ME <mebener44@gmail.com>
2021-04-11 12:47:25 -0700

    stand alone alerts added

--- Develop/script.js
+++ Develop/script.js
@@ -1,35 +1,66 @@
 // Assignment code here
 //Added
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate"); 
+var capitalLetters = confirm ("Do you want capital letters?");
+
+if (capitalLetters) { 
+  alert ("Your password may contain capital letters");
+}
+else{
+  alert ("Your password will contain only lower case letters");
+
+return capitalLetters;
+}
+
+var symbolsAllowed = confirm ("Do you want to use symbols?")
+
+if (symbolsAllowed) {
+  alert("Your password may contain symbols")
+}
+else {
+  alert("Your password will not contain symbols")
+}
 
 
-// Write password to the #password input
-function writePassword() {
+
+
+
+
+
+// Get references to the #generate element (existing)
+//var generateBtn = document.querySelector("#generate"); 
+
+
+
+
+
+// Write password to the #password input (existing)
+/*function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-}
+}*/
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Add event listener to generate button (existing)
+//generateBtn.addEventListener("click", writePassword); 
+
 
 //Function to alert when button is clicked //
 
 
-var capitalLetters = function () {
+// var x = window.confirm("Do you want capital letters?")
+  // confirm capital true or false (I added)
+/*var capitalLetters = function () {
   var capital = "";
   capital = true;
 window.confirm("Do you want capital letters?");
 return capital;
 
-}
+}*/
 
-// var x = window.confirm("Do you want capital letters?")
-  // confirm capital true or false
+
 // var x = window.confirm("Do you want to use symbols?")
   //confirm symbols true or false
 // var x = prompt("Choose a password length between 8 & 120")
