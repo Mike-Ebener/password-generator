737dab321f59fa2bfecc97c5a7799b19f1682fc7
ME <mebener44@gmail.com>
2021-04-11 10:18:07 -0700

    added comments to frame up new code

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,7 @@
 //Added
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate"); 
 
 // Write password to the #password input
 function writePassword() {
@@ -15,3 +15,16 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Function to alert when button is clicked //
+
+
+
+
+//alerts to reference including case, symbols & length
+
+
+
+
+
+var generateBtn = window.alert("You need to capitalize");
